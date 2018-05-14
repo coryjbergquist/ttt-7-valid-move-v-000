154bc87d109ea19db94287544e15f3fda209f4ae
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-14 16:12:17 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,15 +2,6 @@ def valid_move?(board, index)
     index.between?(0, 8) && !position_taken?(board, index) 
  end
  
- 
- 
- def valid_move?(board, index)
-  if board[index] == " "
-    "true"
-  elsif position_taken?(board, index)
-    
- end
-end
 
 def position_taken?(board, index)
   !(board[index] == " " || board[index] == "" || board[index] == nil)
